8b82b2d82f66ad42a82418d86819476d22b6b266
Ray <renqiangqiang.china@gmail.com>
2019-03-13 20:59:13 +0800

    增加demo01

--- lib/demo02.dart
+++ lib/demo02.dart
@@ -9,92 +9,49 @@ class Demo02MaterialApp extends MaterialApp {
             title: 'image detail demo',
             home: ImageDetail(),
             theme: ThemeData(
-              primaryColor: Colors.teal,
+              primaryColor: Colors.blue,
             ));
 }
 
-class ImageDetail extends StatefulWidget {
-
-//  @override
-//  Widget build(BuildContext context) {
-//    var title = Container(
-//      padding: const EdgeInsets.all(32),
-//      child: Row(
-//        children: <Widget>[
-//          Expanded(
-//            child: Column(
-//              crossAxisAlignment: CrossAxisAlignment.start,
-//              children: <Widget>[
-//                Container(
-//                  padding: const EdgeInsets.only(bottom: 8),
-//                  child: Text(
-//                    "Oeschinen Lake Campground",
-//                    style: TextStyle(fontWeight: FontWeight.bold),
-//                  ),
-//                ),
-//                Text(
-//                  'Kandersteg, Switzerland',
-//                  style: TextStyle(color: Colors.grey[500]),
-//                ),
-//              ],
-//            ),
-//          ),
-//          Icon(
-//            Icons.star,
-//            color: Colors.red[500],
-//          ),
-//          Text('41')
-//        ],
-//      ),
-//    );
-//    Widget titleSection = new Container(
-//      padding: const EdgeInsets.all(32.0),
-//      child: new Row(
-//        children: [
-//          new Expanded(
-//            child: new Column(
-//              crossAxisAlignment: CrossAxisAlignment.start,
-//              children: [
-//                new Container(
-//                  padding: const EdgeInsets.only(bottom: 8.0),
-//                  child: new Text(
-//                    'Oeschinen Lake Campground',
-//                    style: new TextStyle(
-//                      fontWeight: FontWeight.bold,
-//                    ),
-//                  ),
-//                ),
-//                new Text(
-//                  'Kandersteg, Switzerland',
-//                  style: new TextStyle(
-//                    color: Colors.grey[500],
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ),
-//          new Icon(
-//            Icons.star,
-//            color: Colors.red[500],
-//          ),
-//          new Text('41'),
-//        ],
-//      ),
-//    );
-//    return Center(
-//      child: Text("dadsdasdsd"),);
-//  }
-
-  @override
-  State<StatefulWidget> createState() {
-    return ImageDetailState();
-  }
-}
-
-class ImageDetailState extends State<ImageDetail>{
+class ImageDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Text("dadsdasdsd"),);
+    return Scaffold(
+      appBar: AppBar(title: Text("ImageDetail"), centerTitle: true,),
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    "Oeschinen Lake Campground",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41')
+        ],
+      ),
+    );
   }
 }
